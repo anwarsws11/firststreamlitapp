0195ef55d7349d721be2727263545696954ac5fe
anwar <103959189+anwarsws11@users.noreply.github.com>
2022-04-21 14:13:15 +0530

    Update streamlit_app.py
    Conditional Loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,17 +39,20 @@ streamlit.dataframe(fruits_to_show)
 # New Section to Display fruityvice API Responce
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('what fruit would you like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
+try:
+  fruit_choice = streamlit.text_input('what fruit would you like information about?')
+  if not fruitchoice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
 # import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
-
 # take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+       streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+streamlit.error()
 
 
 # streamlit.stop()
