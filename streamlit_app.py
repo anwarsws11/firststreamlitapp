908e522b8aec17f4765b73bdea3861de0c70f894
anwar <103959189+anwarsws11@users.noreply.github.com>
2022-04-25 00:03:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ streamlit.header("The Fruit load list contains:")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from fruit_load_list")
-         return my_cur.fetch(my_data_rows)
+         return my_cur.fetch()
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
